cdc74fffe12a7b816dd18182b3afab0fe7f5fbf2
Jeremy Stryer <jm.stryer@gmail.comgit config --global user.name Jeremy>
2021-02-21 15:02:13 -0600

    Add modal

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let templates = {};
+  class App {
+    constructor() {
+      // this.essay = new Essay();
+      this.init();
+    }
 
-  document.querySelectorAll("script[type='text/x-handlebars']").forEach(tmpl => {
-    templates[tmpl["id"]] = Handlebars.compile(tmpl["innerHTML"]);
-  });
+    init() {
+      this.addEventListeners();
+    }
 
-  selectQuestion();
+    addEventListeners() {
+      this.newQuestionBtn = document.querySelector("#new-question-btn");
+      this.newQuestionBtn.addEventListener("click", () => {
+        this.selectQuestion();
+      });
+    }
+    
+    insertQuestion(question) {
+      let questionSection = document.getElementById("question-section");
+      let modal = document.getElementById("modal");
+      let modalContent = document.getElementById("modal-content");
 
-  function selectQuestion() {
-    fetch("./test-questions.json")
-    .then(response => response.json())
-    .then(json => insertQuestion(random(json.questions)));
+      this.processTemplates();
+      // questionSection.innerHTML = this.templates["question-template"]({question});
+
+      modal.style.display = "block";
+      // modalContent.innerHTML = this.templates["question-template"]({question});
+      modalContent.insertAdjacentHTML('afterbegin', this.templates["question-template"]({question}));
+    }
+
+    selectQuestion() {
+      fetch("./test-questions.json")
+      .then(response => response.json())
+      .then(json => this.insertQuestion(Utilities.getRandom(json.questions)));
+    }
+
+    processTemplates() {
+      this.templates = {};
+      document.querySelectorAll("script[type='text/x-handlebars']").forEach(tmpl => {
+        this.templates[tmpl["id"]] = Handlebars.compile(tmpl["innerHTML"]);
+      });
+    }  
   }
 
-  function random(allQuestions) {
-    let index = Math.floor(Math.random() * allQuestions.length);
-    return allQuestions[index];
+  class Essay {
+
   }
 
-  function insertQuestion(question) {
-    let questionSection = document.getElementById("question-section");
-    questionSection.innerHTML = templates["question-template"]({question});
+  class Utilities {
+    static getRandom(questions) {
+      let index = Math.floor(Math.random() * questions.length);
+      return questions[index];
+    }
   }
+
+  new App();
 });
