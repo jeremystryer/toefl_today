69c38c8927397c80e204fb68a183460bb9a707c2
Jeremy Stryer <jm.stryer@gmail.comgit config --global user.name Jeremy>
2021-02-26 18:23:04 -0600

    Change layout, add start button, replace textarea with contenteditable div

--- app.js
+++ app.js
@@ -5,11 +5,102 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     init() {
-      this.selectQuestion();
-      this.bind();
-      setTimeout(this.startTimer.bind(this), 1);
+      this.addEventListeners();
     }
 
+    showTimeEndedModal() {
+      let modalReview = document.querySelector("#modal-review");
+      let reviewBtn = document.querySelector("#review-button");
+      modalReview.style.display = "block";
+      
+      reviewBtn.addEventListener('click', () => {
+        
+      });
+
+    }
+
+    addEventListeners() {
+      let startBtn = document.querySelector(".start");
+
+      startBtn.addEventListener("click", () => {
+        let modalStart = document.querySelector("#modal-start");
+        modalStart.style.display = "none";
+        this.essay = new Essay();
+        // setTimeout(this.essay.startTimer.bind(this), 1);
+      });
+    }
+
+    // checkWordCount() {
+    //   let checkWordCountBtn = document.querySelector("#check-word-count-button");
+      
+    //   checkWordCountBtn.addEventListener('click', () => {
+    //     let text = document.querySelector("textarea").value;
+    //     let numWords = text.match(/(\w|')*(\w|\-)+/gi).length;
+
+    //     alert(numWords);
+    //   });
+    // }
+
+    // getNewQuestion() {
+    //   let getNewQuestionBtn = document.querySelector("#get-new-question-button");
+
+    //   getNewQuestionBtn.addEventListener('click', () => {
+
+    //   });
+    // }
+    
+
+  }
+
+  class Essay {
+    constructor() {
+      this.init();
+    }
+
+    init() {
+      this.selectQuestion();
+      this.setTab();
+    }
+
+    insertQuestion(question) {
+      let questionContainer = document.querySelector("#question-container");
+      this.processTemplates();
+      questionContainer.innerHTML = this.templates["question-template"]({question});
+      // debugger;
+    }
+
+    selectQuestion() {
+      fetch("./test-questions.json")
+      .then(response => response.json())
+      .then(json => this.insertQuestion(Utilities.getRandom(json.questions)));
+    }
+
+    processTemplates() {
+      this.templates = {};
+
+      document.querySelectorAll("script[type='text/x-handlebars']").forEach(tmpl => {
+        this.templates[tmpl["id"]] = Handlebars.compile(tmpl["innerHTML"]);
+      });
+    }
+
+    setTab() {
+      let essayArea = document.querySelector("#essay-area");
+
+      essayArea.addEventListener('keydown', function(e) {
+        if (e.key == 'Tab') {
+          e.preventDefault();
+          var start = this.selectionStart;
+          var end = this.selectionEnd;
+      
+          this.value = this.value.substring(0, start) +
+            "\t" + this.value.substring(end);
+      
+          this.selectionStart =
+            this.selectionEnd = start + 1;
+        }
+      });
+    }
+    
     startTimer(){      
       let seconds = 1800;
       let countDiv = document.getElementById("timer");
@@ -29,8 +120,8 @@ document.addEventListener('DOMContentLoaded', () => {
           min = '0' + min;
         }
       
-      let textArea = document.querySelector("textarea");
-      textArea.disabled = false;
+      // let textArea = document.querySelector("textarea");
+      // textArea.disabled = false;
 
       countDiv.innerHTML = min + ":" + remainingSeconds;
         
@@ -43,86 +134,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
     }
-
-    showTimeEndedModal() {
-      let modal = document.querySelector("#modal");
-      let reviewBtn = document.querySelector("#review-button");
-      modal.style.display = "block";
-      
-      reviewBtn.addEventListener('click', () => {
-        
-      });
-
-    }
-
-    bind() {
-      this.setTab();
-      this.checkWordCount();
-      this.getNewQuestion();
-    }
-
-    checkWordCount() {
-      let checkWordCountBtn = document.querySelector("#check-word-count-button");
-      
-      checkWordCountBtn.addEventListener('click', () => {
-        let text = document.querySelector("textarea").value;
-        let numWords = text.match(/(\w|')*(\w|\-)+/gi).length;
-
-        alert(numWords);
-      });
-    }
-
-    getNewQuestion() {
-      let getNewQuestionBtn = document.querySelector("#get-new-question-button");
-
-      getNewQuestionBtn.addEventListener('click', () => {
-
-      });
-    }
-
-    setTab() {
-      let textArea = document.querySelector('textarea');
-
-      textArea.addEventListener('keydown', function(e) {
-        if (e.key == 'Tab') {
-          e.preventDefault();
-          var start = this.selectionStart;
-          var end = this.selectionEnd;
-      
-          this.value = this.value.substring(0, start) +
-            "\t" + this.value.substring(end);
-      
-          this.selectionStart =
-            this.selectionEnd = start + 1;
-        }
-      });
-    }
-    
-    insertQuestion(question) {
-      let questionPlacement= document.getElementById("question");
-      let essayArea = document.getElementById("essay-area");
-
-      this.processTemplates();
-      questionPlacement.innerHTML = this.templates["question-template"]({question});
-    }
-
-
-    selectQuestion() {
-      fetch("./test-questions.json")
-      .then(response => response.json())
-      .then(json => this.insertQuestion(Utilities.getRandom(json.questions)));
-    }
-
-    processTemplates() {
-      this.templates = {};
-      document.querySelectorAll("script[type='text/x-handlebars']").forEach(tmpl => {
-        this.templates[tmpl["id"]] = Handlebars.compile(tmpl["innerHTML"]);
-      });
-    }  
-  }
-
-  class Essay {
-
   }
 
   class Utilities {
